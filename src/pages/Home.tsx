c199385f77455ab0b6a02f2f9d0063693356b750
havorx <55874008+jazzkatxlemon@users.noreply.github.com>
2021-11-18 01:38:54 +0700

    add idea + reformat code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,70 +1,71 @@
 import {
-  IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
-  IonItem, IonList, IonRefresherContent, IonRefresher,
-  IonSearchbar
+    IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
+    IonItem, IonList, IonRefresherContent, IonRefresher,
+    IonSearchbar
 } from '@ionic/react';
-import { useEffect, useState } from 'react';
-import { getAllRecords } from '../databaseHandler';
-import { Record } from '../models';
+import {useEffect, useState} from 'react';
+import {getAllRecords} from '../databaseHandler';
+import {Record} from '../models';
+import ReactAudioPlayer from 'react-audio-player';
 
 const Home: React.FC = () => {
-  const [records, setRecords] = useState<any[]>([])
-  const [searchText, setText] = useState('')
+    const [records, setRecords] = useState<any[]>([]);
+    const [searchText, setText] = useState('');
 
-  async function fetchData() {
-    const allRecords = await getAllRecords();
-    setRecords(allRecords);
-    console.log(allRecords);
-  }
+    async function fetchData() {
+        const allRecords = await getAllRecords();
+        setRecords(allRecords);
+        console.log(allRecords);
+    }
 
-  async function searchProperty(searchText: string) {
-    const allRecords = await getAllRecords() as Record[];
-    searchText = searchText.toLowerCase();
-    const filteredResults = allRecords.filter(record => record.property.toLowerCase().indexOf(searchText) > - 1);
-    setRecords(filteredResults);
-  }
+    async function searchProperty(searchText: string) {
+        const allRecords = await getAllRecords() as Record[];
+        searchText = searchText.toLowerCase();
+        const filteredResults = allRecords.filter(record => record.property.toLowerCase().indexOf(searchText) > -1);
+        setRecords(filteredResults);
+    }
 
-  useEffect(() => {
-    fetchData();
-  }, [])
+    useEffect(() => {
+        fetchData()
+    }, [])
 
 
-  useEffect(() => {
-    searchProperty(searchText);
-  }, [searchText])
+    useEffect(() => {
+        searchProperty(searchText);
+    }, [searchText])
 
-  function doRefresh(event: any) {
-    fetchData();
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      event.detail.complete();
-    }, 1000);
-  }
+    function doRefresh(event: any) {
+        fetchData();
+        setTimeout(() => {
+            console.log('Async operation has ended');
+            event.detail.complete();
+        }, 1000);
+    }
 
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>CourseWork</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonSearchbar value={searchText} onIonChange={e => setText(e.detail.value!)}></IonSearchbar>
-        <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
-          <IonRefresherContent></IonRefresherContent>
-        </IonRefresher>
-        {records &&
-          <IonList>
-            {
-              records.map((c, i) =>
-                <IonItem routerLink={'details/' + c.id} button key={i}>{c.property}</IonItem>
-              )
-            }
-          </IonList>
-        }
-      </IonContent>
-    </IonPage >
-  );
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>CourseWork</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonSearchbar value={searchText} onIonChange={e => setText(e.detail.value!)}></IonSearchbar>
+                <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
+                    <IonRefresherContent></IonRefresherContent>
+                </IonRefresher>
+                {records &&
+                <IonList>
+                    {
+                        records.map((c, i) =>
+                            <IonItem routerLink={'details/' + c.id} button key={i}>{c.property}</IonItem>
+                        )
+                    }
+                </IonList>
+                }
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Home;
